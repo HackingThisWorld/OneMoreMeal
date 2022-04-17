75a6dc2e514a551d60461e80963e8284473b0a6a
st1llwater <alokpunjofficial@gmail.com>
2022-04-17 13:53:48 +0530

    styling

--- components/Text.tsx
+++ components/Text.tsx
@@ -6,8 +6,9 @@ const Text = () => {
       <section className="text-gray-600 mt-16 body-font">
         <div className="container px-5 mx-auto">
           <div className="flex flex-col text-center w-full">
-            <h1 className="sm:text-5xl pb-2 border-b-4 lg:mx-24 border-gray-600 text-2xl font-medium title-font mb-4 text-gray-900">
-              Can you imagine a world without hunger?
+            <h1 className="sm:text-5xl font-mw pb-2 border-b-4 lg:mx-24 border-orange-700 text-2xl font-medium title-font mb-4 text-gray-900">
+              Can you imagine a world without{' '}
+              <span className="text-orange-500">hunger?</span>
             </h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-lg">
               There are people in this world so hungry that god cannot appear to
